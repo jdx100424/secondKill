e513e653ee69ae70e00c4b4847cbda402c338973
15914380151@139.com <15914380151@139.com>
2017-04-09 17:54:46 +0800

    red package

---
+++ secondKill-service/src/main/java/com/maoshen/secondkill/service/RedPackageService.java
@@ -0,0 +1,40 @@
+package com.maoshen.secondkill.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.maoshen.secondkill.service.vo.LotteryRecordDto;
+
+/**
+ * 发指定的红包给指定的人
+ * 
+ * @author jdx
+ *
+ */
+public interface RedPackageService {
+	/**
+	 * 生成红包，
+	 * @param redPackageCount，红包数
+	 * @param money，总金额
+	 * @param isAllSame，红包里面的前是否一致
+	 * @param userIdList，参与抢的用户ID集合
+	 * @return 事件ID
+	 * @throws Exception
+	 */
+	public Long create(int redPackageCount,BigDecimal money,boolean isAllSame,List<Long> userIdList) throws Exception;
+	/**
+	 * 抢票
+	 * 
+	 * @param userId,
+	 * @param eventId,
+	 * @return
+	 */
+	public LotteryRecordDto draw(Long userId,Long eventId) throws Exception;
+
+	/**
+	 * 查询指定用户中奖记录
+	 * 
+	 * @return
+	 */
+	public List<LotteryRecordDto> getRecordByUserId(Long userId,Long eventId);
+}
