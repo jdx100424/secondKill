4551edac228d2f2f2b76e1996e608ebf877030f8
daxian.jiang@lifesense.com <daxian.jiang@lifesense.com>
2017-04-10 11:58:29 +0800

    微信抢红包算法

--- secondKill-service-impl/src/test/java/com/maoshen/secondkill/service/impl/test/CommonsServiceImplTest.java
+++ secondKill-service-impl/src/test/java/com/maoshen/secondkill/service/impl/test/CommonsServiceImplTest.java
@@ -42,4 +42,35 @@ public class CommonsServiceImplTest extends BaseTest {
 			}.start();
 		}
 	}
+	
+	public static void main(String []s){
+		CommonsServiceImplTest commonsServiceImplTest =new CommonsServiceImplTest();
+		LeftMoneyPackage leftMoneyPackage = new LeftMoneyPackage();
+		leftMoneyPackage.setRemainMoney(10);
+		leftMoneyPackage.setRemainSize(10);
+		double d = 0;
+		for(int i=0;i<10;i++){
+			double result =  commonsServiceImplTest.getRandom(leftMoneyPackage);
+			d = d + result;
+			System.out.println(result);
+		}
+		System.out.println("result:"+d);
+	}
+	
+	
+	public double getRandom(LeftMoneyPackage _leftMoneyPackage){
+		if (_leftMoneyPackage.remainSize == 1) {
+			_leftMoneyPackage.remainSize--;
+			return (double)Math.round(_leftMoneyPackage.remainMoney*100)/100;
+		}
+		Random r = new Random();
+		double min = 0.01;
+		double max = _leftMoneyPackage.remainMoney/_leftMoneyPackage.remainSize*2;
+		double money = r.nextDouble()*max;
+		money = money<=min?0.01:money;
+		money = Math.floor(money*100)/100;
+		_leftMoneyPackage.remainSize--;
+		_leftMoneyPackage.remainMoney = _leftMoneyPackage.remainMoney-money;
+		return money;
+	} 
 }
---
+++ secondKill-service-impl/src/test/java/com/maoshen/secondkill/service/impl/test/LeftMoneyPackage.java
@@ -0,0 +1,18 @@
+package com.maoshen.secondkill.service.impl.test;
+
+public class LeftMoneyPackage {
+	public int remainSize;
+	public double remainMoney;
+	public int getRemainSize() {
+		return remainSize;
+	}
+	public void setRemainSize(int remainSize) {
+		this.remainSize = remainSize;
+	}
+	public double getRemainMoney() {
+		return remainMoney;
+	}
+	public void setRemainMoney(double remainMoney) {
+		this.remainMoney = remainMoney;
+	}
+}
